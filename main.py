f1d555f0fc91c3b081db3e8eaaa53cf292019bb7
River.Shi <nachuan.shi.quant@gamil.com>
2025-12-23 11:19:30 +0000

    style: Improve code formatting and readability in main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from nanobt.utils import preprocess_data
 from nanobt.types import record_dtype, INVALID_MIN, INVALID_MAX
 from nanobt.stats import LinearAssetRecord
 
+
 @njit
 def accelerated_backtest(
     relative_half_spread,
@@ -25,7 +26,7 @@ def accelerated_backtest(
     best_bid_tick_ack,
     best_ask_tick_ack,
     bid_fill_tick_after_ack,
-    ask_fill_tick_after_ack
+    ask_fill_tick_after_ack,
 ):
     # req_bid_tick: bid order price in ticks (limit buy order with GTX) sent to the exchange, before the exchange acknowledges it.
     # req_ask_tick: ask order price in ticks (limit sell order with GTX) sent to the exchange, before the exchange acknowledges it.
@@ -59,7 +60,7 @@ def accelerated_backtest(
     record = np.empty(len(local_ts), record_dtype)
 
     while True:
-        #--------------------------------------------------------
+        # --------------------------------------------------------
         # Local bot logic at `local_ts[t]`.
         mid_tick = (best_bid_tick[t] + best_ask_tick[t]) / 2.0
         mid_px = mid_tick * tick_size
@@ -70,10 +71,16 @@ def accelerated_backtest(
         relative_bid_depth = relative_half_spread + skew * normalized_position
         relative_ask_depth = relative_half_spread - skew * normalized_position
 
-        req_bid_tick = min(np.floor(mid_tick * (1.0 - relative_bid_depth)), best_bid_tick[t])
-        req_ask_tick = max(np.ceil(mid_tick * (1.0 + relative_ask_depth)), best_ask_tick[t])
+        req_bid_tick = min(
+            np.floor(mid_tick * (1.0 - relative_bid_depth)), best_bid_tick[t]
+        )
+        req_ask_tick = max(
+            np.ceil(mid_tick * (1.0 + relative_ask_depth)), best_ask_tick[t]
+        )
 
-        req_bid_qty = req_ask_qty = max(round(order_notional_value / mid_px / lot_size) * lot_size, lot_size)
+        req_bid_qty = req_ask_qty = max(
+            round(order_notional_value / mid_px / lot_size) * lot_size, lot_size
+        )
 
         # If the position exceeds the risk limit (max notional position),
         # no orders shall be open in that direction.
@@ -82,7 +89,7 @@ def accelerated_backtest(
         if normalized_position < -1:
             req_ask_tick = INVALID_MAX
 
-        #--------------------------------------------------------
+        # --------------------------------------------------------
         # Records the current state.
         record[rec_i].timestamp = local_ts[t]
         record[rec_i].price = mid_tick * tick_size
@@ -95,7 +102,7 @@ def accelerated_backtest(
 
         rec_i += 1
 
-        #--------------------------------------------------------
+        # --------------------------------------------------------
         # Processes the exchange-side logic (order fill logic).
 
         # If any of the requested order prices differ from the open order's price,
@@ -146,7 +153,10 @@ def accelerated_backtest(
             # If there are open orders with valid prices,
             # checks whether they are filled before the next local timestamp (`local_ts[t+n]`)
             # that is greater than the current timestamp (`order_ack_ts[t]`).
-            if open_bid_tick > INVALID_MIN and open_bid_tick >= bid_fill_tick_after_ack[t]:
+            if (
+                open_bid_tick > INVALID_MIN
+                and open_bid_tick >= bid_fill_tick_after_ack[t]
+            ):
                 execute_value = open_bid_tick * open_bid_qty
                 balance -= execute_value
                 position += open_bid_qty
@@ -155,7 +165,10 @@ def accelerated_backtest(
                 trading_value += execute_value
                 # Invalidates the price because the order is filled.
                 open_bid_tick = INVALID_MIN
-            if open_ask_tick < INVALID_MAX and open_ask_tick <= ask_fill_tick_after_ack[t]:
+            if (
+                open_ask_tick < INVALID_MAX
+                and open_ask_tick <= ask_fill_tick_after_ack[t]
+            ):
                 execute_value = open_ask_tick * open_ask_qty
                 balance += execute_value
                 position -= open_ask_qty
@@ -202,66 +215,82 @@ def accelerated_backtest(
 
     return record[:rec_i]
 
+
 def main():
     tick_size = 0.1
     lot_size = 0.001
+
+    # if you do not have enough memory, use the code bellow
+    # table = preprocess_data(
+    #     tick_size=tick_size,
+    #     interval_ns=1_000_000_000,
+    #     start_date=datetime(2025, 11, 5, tzinfo=UTC),
+    #     end_date=datetime(2025, 11, 6, tzinfo=UTC),
+    #     trades_files=[
+    #         "/share/tardis/raw/binance-futures/trades/BTCUSDT/binance-futures_trades_2025-11-05_BTCUSDT.parquet"
+    #     ],
+    #     book_ticker_files=[
+    #         "/share/tardis/raw/binance-futures/book_ticker/BTCUSDT/binance-futures_book_ticker_2025-11-05_BTCUSDT.parquet"
+    #     ],
+    # )
+
     table = preprocess_data(
         tick_size=tick_size,
         interval_ns=1_000_000_000,
         start_date=datetime(2025, 11, 5, tzinfo=UTC),
         end_date=datetime(2025, 11, 10, tzinfo=UTC),
+        base_dir="/share/tardis/raw",
+        exchange="binance-futures",
+        symbol="BTCUSDT"
     )
-    
-    local_ts = table['local_ts'].to_numpy()
-    best_bid_tick = table['best_bid_tick'].to_numpy()
-    best_ask_tick = table['best_ask_tick'].to_numpy()
-    bid_fill_tick = table['bid_fill_tick'].to_numpy()
-    ask_fill_tick = table['ask_fill_tick'].to_numpy()
-    order_ack_ts = table['order_ack_ts'].to_numpy()
-    bid_fill_tick_ack = table['bid_fill_tick_ack'].to_numpy()
-    ask_fill_tick_ack = table['ask_fill_tick_ack'].to_numpy()
-    best_bid_tick_ack = table['best_bid_tick_ack'].to_numpy()
-    best_ask_tick_ack = table['best_ask_tick_ack'].to_numpy()
-    bid_fill_tick_after_ack = table['bid_fill_tick_after_ack'].to_numpy()
-    ask_fill_tick_after_ack = table['ask_fill_tick_after_ack'].to_numpy()
+
+    local_ts = table["local_ts"].to_numpy()
+    best_bid_tick = table["best_bid_tick"].to_numpy()
+    best_ask_tick = table["best_ask_tick"].to_numpy()
+    bid_fill_tick = table["bid_fill_tick"].to_numpy()
+    ask_fill_tick = table["ask_fill_tick"].to_numpy()
+    order_ack_ts = table["order_ack_ts"].to_numpy()
+    bid_fill_tick_ack = table["bid_fill_tick_ack"].to_numpy()
+    ask_fill_tick_ack = table["ask_fill_tick_ack"].to_numpy()
+    best_bid_tick_ack = table["best_bid_tick_ack"].to_numpy()
+    best_ask_tick_ack = table["best_ask_tick_ack"].to_numpy()
+    bid_fill_tick_after_ack = table["bid_fill_tick_after_ack"].to_numpy()
+    ask_fill_tick_after_ack = table["ask_fill_tick_after_ack"].to_numpy()
 
     relative_half_spread = 0.00025
     skew = relative_half_spread
     order_notional_value = 50000
     max_notional_position = order_notional_value * 20
-    fee_per_value = -0.00005 # 0.005% rebates
+    fee_per_value = -0.00005  # 0.005% rebates
 
     record = accelerated_backtest(
-            relative_half_spread,
-            skew,
-            order_notional_value,
-            max_notional_position,
-            fee_per_value,
-            tick_size,
-            lot_size,
-            local_ts,
-            best_bid_tick,
-            best_ask_tick,
-            bid_fill_tick,
-            ask_fill_tick,
-            order_ack_ts,
-            bid_fill_tick_ack,
-            ask_fill_tick_ack,
-            best_bid_tick_ack,
-            best_ask_tick_ack,
-            bid_fill_tick_after_ack,
-            ask_fill_tick_after_ack
+        relative_half_spread,
+        skew,
+        order_notional_value,
+        max_notional_position,
+        fee_per_value,
+        tick_size,
+        lot_size,
+        local_ts,
+        best_bid_tick,
+        best_ask_tick,
+        bid_fill_tick,
+        ask_fill_tick,
+        order_ack_ts,
+        bid_fill_tick_ack,
+        ask_fill_tick_ack,
+        best_bid_tick_ack,
+        best_ask_tick_ack,
+        bid_fill_tick_after_ack,
+        ask_fill_tick_after_ack,
     )
 
     stats = (
-        LinearAssetRecord(record)
-            .resample('1s')
-            .stats(book_size=max_notional_position)
+        LinearAssetRecord(record).resample("1s").stats(book_size=max_notional_position)
     )
     print(stats.summary())
     fig = stats.plot()
-    fig.savefig('backtest_result.png')
-
+    fig.savefig("backtest_result.png")
 
 
 if __name__ == "__main__":
